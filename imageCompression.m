6dfeb5c7d20467d1ed3c3c7b99ddb74191bc5d63
Robin <robin_ronson@yahoo.com>
2017-10-26 22:31:06 -0500

    added script for producing random centeroids

--- imageCompression.m
+++ imageCompression.m
@@ -1,7 +1,23 @@
 % Read the image as a 3 dimensional matrix
 
-A = double(imread('bird_small.tiff')); 
+clc; 
+ 
 
-X = randsample(A, 1); 
+A = double(imread('bird_small.tiff'));
 
-display(X); 
+% create 16 random centeroids
+
+
+dim = size(A, 1); 
+
+k = 16; 
+
+centers = zeros(k, 3);  
+
+x_rand = ceil(dim*rand(dim,1))
+
+y_rand = ceil(dim*rand(dim,1))
+
+for i = 1:k
+  centers(i,:) = A(x_rand(i), y_rand(i),:); 
+end
